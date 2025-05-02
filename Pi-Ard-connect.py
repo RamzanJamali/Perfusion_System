5debfde3bed6af0013eef86924e3c55dc9d61ca8
Ramzan Jamali <ramanjamali@gmail.com>
2025-05-02 15:30:48 +0200

    These files connect arduino with raspberry pi via usb cable

---
+++ Pi-Ard-connect.py
@@ -0,0 +1,16 @@
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+time.sleep(2)
+
+def send_command(command):
+    ser.write((command + '\n').encode())
+    response = ser.readline().decode().strip()
+    print(f"Arduino response: {response}")
+    
+while True:
+    send_command("LED_ON")
+    time.sleep(0.5)
+    send_command("LED_OFF")
+    time.sleep(0.5)
