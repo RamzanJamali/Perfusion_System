b0abda2001c3241674ef525dcc8cf812ccf1a6fe
ramzanjamali <ramanjamali@gmail.com>
2025-05-12 16:32:12 +0200

    New files for motor driver added.

--- Pi-Ard-connect.py
+++ Pi-Ard-connect.py
@@ -1,7 +1,8 @@
-import serial
+from serial import Serial
 import time
 
-ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+# ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser = Serial(port='COM7', baudrate=9600, timeout=1)
 time.sleep(2)
 
 def send_command(command):
