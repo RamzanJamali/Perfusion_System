98edbaa8e8e2db78176027566fa786a847e3040d
ramzanjamali <ramanjamali@gmail.com>
2025-08-25 18:00:35 +0200

    Minor bug fixes in run scripts

--- Run/Pi_run_dashboard.sh
+++ Run/Pi_run_dashboard.sh
@@ -2,6 +2,6 @@
 # Make it executable by running following command in bash:
 # chmod +x Pi_run_dashboard.sh
 # — Activate your venv (if you use one) —
-source /home/AG-Lang/Downloads/Perfusion_System/venv/bin/activate
+source /home/AG-Lang/Downloads/Perfusion_System/.venv/bin/activate
 # — Run your Streamlit app —
 streamlit run "/home/AG-Lang/Downloads/Perfusion_System/Dashboard/dashboard.py" --server.port 8503
