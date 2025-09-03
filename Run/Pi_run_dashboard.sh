c3aedb0f6b50cae48986e1c23d5ddd37ef4289da
Ramzan Jamali <ramanjamali@gmail.com>
2025-09-03 10:40:50 +0200

    Stop tracking Run folder

--- Run/Pi_run_dashboard.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Make it executable by running following command in bash:
-# chmod +x Pi_run_dashboard.sh
-# — Activate your venv (if you use one) —
-source /home/AG-Lang/Downloads/Perfusion_System/.venv/bin/activate
-# — Run your Streamlit app —
-streamlit run "/home/AG-Lang/Downloads/Perfusion_System/Dashboard/dashboard.py" --server.port 8503
