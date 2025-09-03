df799467915bb772ea4645b06042d30ebad2c25e
ramzanjamali <ramanjamali@gmail.com>
2025-09-03 13:20:24 +0200

    Repository management

--- Run/Windows_run_csv.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM — Activate your venv (if you use one); otherwise skip these two lines —
-call E:\Perfusion_System\.venv\Scripts\activate.bat
-REM — Run your Streamlit app —
-streamlit run "E:\Perfusion_System\save_csv\csv.py" --server.port 8502
-pause
