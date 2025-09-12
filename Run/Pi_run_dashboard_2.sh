896e9528dfffd471752d3a8b5b581232befacb3d
Ramzan Jamali <ramanjamali@gmail.com>
2025-09-12 10:32:33 +0200

    venv fixes

--- Run/Pi_run_dashboard_2.sh
+++ Run/Pi_run_dashboard_2.sh
@@ -2,6 +2,6 @@
 # Make it executable by running following command in bash:
 # chmod +x Pi_run_dashboard.sh
 # — Activate your venv (if you use one) —
-source /home/AG-Lang/Downloads/Perfusion_System/.venv/bin/activate
+source /home/AG-Lang/Downloads/Perfusion_System/venv/bin/activate
 # — Run your Streamlit app —
 python "/home/AG-Lang/Downloads/Perfusion_System/Dashboard_2/dashboard_2.py"
